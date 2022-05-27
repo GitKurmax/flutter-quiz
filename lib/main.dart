13324ecf2ce37c884b3dac5233b3e06da3dbfb26
alex <oleksandr.kurmoiarov@smartexe.com>
2022-05-27 17:49:05 +0300

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('First app')),
+      body: Column(children: <Widget>[
+        Text('The question'),
+        ElevatedButton(
+            onPressed: () => {},
+            child: Text('Answer 1')),
+        ElevatedButton(
+            onPressed: null,
+            child: Text('Answer 2')),
+        ElevatedButton(
+            onPressed: null,
+            child: Text('Answer 3')),
+      ]),
+    ));
+  }
+}
